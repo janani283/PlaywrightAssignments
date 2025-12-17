8821d21d8188fb8cf1be613406e98a466ede5f15
janani283 <jananisjmhss@gmail.com>
2025-12-17 20:54:47 +0530

    Add files via upload

---
+++ Day1HomeAssignment.js
@@ -0,0 +1,11 @@
+let testerName='janani'
+let organizationName='cognizant'
+let contactNumber=9940186973
+let isAutomationTester=true
+let hasPlaywrightExperience
+
+console.log("testerName : "+" " +typeof testerName)
+console.log("organizationName : "+" "+typeof organizationName )
+console.log("contactNumber : "+" "+typeof contactNumber)
+console.log("isAutomationTester : "+" "+typeof isAutomationTester)
+console.log("hasPlaywrightExperience is : "+" "+typeof hasPlaywrightExperience)
